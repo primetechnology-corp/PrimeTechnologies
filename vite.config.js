0f123b405e02eeec097366475277033a5339fb11
Luiz Guilherme <luiz.correira@primetechnology.com.br>
2026-04-28 15:13:56 -0300

    feat: ajustes finais, com processos de minificação e otimização adicionados

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-export default {
-  server: {
-    host: true,
-    allowedHosts: [
-      "impossible-float-crossing-surprising.trycloudflare.com"
-    ]
-  }
-}
+import { defineConfig } from 'vite'
+import viteImagemin from 'vite-plugin-imagemin'
+
+export default defineConfig({
+
+  build: {
+    sourcemap: false,
+    minify: 'esbuild',
+    cssCodeSplit: true,
+    assetsInlineLimit: 4096
+  },
+  plugins: [
+    viteImagemin({
+      mozjpeg: { quality: 70 },
+      pngquant: { quality: [0.6, 0.8] },
+      webp: { quality: 75 }
+    })
+  ]
+})
